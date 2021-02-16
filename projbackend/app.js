4a67aa399651db43798e9c8916f5de0d6cc364bb
arbbwn <54329740+arbazbagwan@users.noreply.github.com>
2021-02-16 10:48:58 +0530

    Update app.js

--- projbackend/app.js
+++ projbackend/app.js
@@ -1,9 +1,19 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 const express = require("express");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const authRoutes = require('./routes/authentication');
 
+//Middleware
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use(cookieParser());
 
+
+//DB Connection
 mongoose.connect(process.env.DATABASE,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -12,8 +22,12 @@ mongoose.connect(process.env.DATABASE,{
     console.log("db connected");
 });
 
-const port = 8000;
+//MyRoutes
 
+app.use("/api", authRoutes);
+
+//Server 
+const port = 8000;
 app.listen(port, ()=>{
     console.log(`app is running at ${port}`);
 })
