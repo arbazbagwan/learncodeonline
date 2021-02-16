aba146e3dfea6d2637b8c6f11dc5b5be810b2803
arbbwn <54329740+arbazbagwan@users.noreply.github.com>
2021-02-16 10:49:23 +0530

    Update user.js

--- projbackend/models/user.js
+++ projbackend/models/user.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 const crypto = require('crypto');
-const uuidv1 = require("uuid/v1");
+const uuid = require("uuid");
 
 var userSchema = new mongoose.Schema({
     name: {
@@ -46,7 +46,7 @@ userSchema
     .virtual("password")
     .set(function(password) {
         this._password = password;
-        this.salt = uuidv1();
+        this.salt = uuid();
         this.encry_password = this.securePassword(password);
 
     })
@@ -54,17 +54,17 @@ userSchema
     return this._password;
 });
 
-userSchema.method = {
+userSchema.methods = {
 
     authenticate: function(plainpassword){
         return this.securePassword(plainpassword)===this.encry_password
     },
 
     securePassword: function(plainpassword){
-        if(!password) return "";
+        if(!plainpassword) return "";
         try{
             return crypto.createHmac('sha256', this.salt)
-            .update('plainpassword')
+            .update(plainpassword)
             .digest("hex");
         }
         catch(err){
